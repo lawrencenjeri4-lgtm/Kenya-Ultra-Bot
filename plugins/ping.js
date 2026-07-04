8c30854e3c3d5c8f57d897a601d7526248d81610
Lucid dev <lawrencenjeri4@gmail.com>
2026-07-04 17:30:44 +0300

    Add Ping command to check bot response speed

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,1 +1,32 @@
+module.exports = {
+    name: "Ping",
+
+    aliases: ["ping", "p"],
+
+    category: "General",
+
+    description: "Check bot response speed.",
+
+    usage: ".ping",
+
+    cooldown: 3,
+
+    owner: false,
+
+    admin: false,
+
+    group: false,
+
+    private: false,
+
+    premium: false,
+
+    async execute(sock, m, args) {
+
+        await m.react("🏓");
+
+        await m.reply("🏓 Pong!");
+
+    }
+};
 
