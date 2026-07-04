baffa472d9f125ff78c6a071d5c7bfe3c347c1bc
Lucid dev <lawrencenjeri4@gmail.com>
2026-07-04 17:25:43 +0300

    Update help.js

--- plugins/help.js
+++ plugins/help.js
@@ -1,1 +1,21 @@
+const { PREFIX } = require("../config");
+
+module.exports = {
+    name: "help",
+    aliases: ["commands"],
+
+    category: "General",
+
+    async execute(sock, m, args) {
+
+        m.reply(
+`Use
+
+${PREFIX}menu
+
+to view all available commands.`
+        );
+
+    }
+};
 
