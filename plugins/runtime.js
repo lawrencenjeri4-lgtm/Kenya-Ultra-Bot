51c1ac47faa8b601fc02612356be0d713ad69d4b
Lucid dev <lawrencenjeri4@gmail.com>
2026-07-04 17:32:41 +0300

    Add runtime command to check bot uptime

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -1,1 +1,21 @@
+const { runtime } = require("../lib/functions");
+
+module.exports = {
+
+    name: "runtime",
+
+    aliases: ["uptime"],
+
+    category: "General",
+
+    desc: "Bot uptime",
+
+    async execute(sock, m, args) {
+
+        await m.reply(
+            `⏳ Uptime\n\n${runtime(process.uptime())}`
+        );
+
+    }
+};
 
